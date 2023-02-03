6483f82fcca811d0780f78a8c481cfc9b39ba191
sso9594 <sso9594@naver.com>
2023-02-03 21:30:52 +0900

    chore(post_server): first commit demo service using docker

--- src/main/java/com/btree/post/repository/salesrepository.java
+++ src/main/java/com/btree/post/repository/salesrepository.java
@@ -1,7 +1,22 @@
 package com.btree.post.repository;
 
 import com.btree.post.entity.salespost;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface salesrepository extends JpaRepository<salespost,Long> {
+   Page<salespost> findAllByOrderBySalespostidDesc(Pageable pageable);
+
+   @Modifying
+   @Query("update salespost a set a.likenum = a.likenum - 1 where a.salespostid = :id")
+   void minusLike (@Param("id") Long id);
+   @Modifying
+   @Query("update salespost a set a.likenum = a.likenum + 1 where a.salespostid = :id")
+   void plusLike (@Param("id") Long id);
 }
