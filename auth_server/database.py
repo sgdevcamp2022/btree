47b14053b72276ea9bf91bc18f92fd9a99bfea8b
wlsdn2749 <wlsdn2749@gmail.com>
2023-02-07 03:20:43 +0900

    chore(integrated): test success between auth and post

--- auth_server/database.py
+++ auth_server/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+import sys
 
 DB_USER = os.environ.get("DATABASE_USERNAME")
 DB_PASSWORD = os.environ.get("DATABASE_PASSWORD")
@@ -13,10 +14,13 @@ DB_PORT = 3306
 # SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
 # SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://{}:{}@{}/{}?port={}?charset=utf8'.format(USER, PASSWORD, HOST, DATABASE, PORT)
 SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}?charset=utf8'
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+try:
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL
+    )
+except:
+    sys.exit(1)
+    
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
