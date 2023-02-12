72d6f5455bd310acf0b256e1a65e4fb6fb6669cf
sso9594 <58455389+sso9594@users.noreply.github.com>
2023-02-13 03:54:21 +0900

    Revert "chore(post_server): gps service push"

--- src/main/java/com/btree/post/repository/commentrepository.java
+++
@@ -1,7 +0,0 @@
-package com.btree.post.repository;
-
-import com.btree.post.entity.boardcomment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface commentrepository extends JpaRepository<boardcomment,Long> {
-}
